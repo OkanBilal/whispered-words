c789342d32e3447afc2068fb4142089848c745df
Okan Bilal Balcı <obbalci@gmail.com>
2023-12-28 19:48:46 +0300

    update rate limit config for upload api

---
+++ app/api/upload/route.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
+
+export async function POST(req: Request) {
+  if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+    const ip = req.headers.get("x-forwarded-for") ?? "127.0.0.1";
+    const ratelimit = new Ratelimit({
+      redis: kv,
+      limiter: Ratelimit.slidingWindow(1, "60 s"),
+    });
+    const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+      ip
+    );
+    if (!success) {
+      return new Response(
+        "You have reached your request limit. Please try again in 1 minutes.",
+        {
+          status: 429,
+          headers: {
+            "X-RateLimit-Limit": limit.toString(),
+            "X-RateLimit-Remaining": remaining.toString(),
+            "X-RateLimit-Reset": reset.toString(),
+          },
+        }
+      );
+    }
+  } else {
+    console.log(
+      "KV_REST_API_URL and KV_REST_API_TOKEN env vars not found, not rate limiting..."
+    );
+  }
+
+  const data = await req.formData();
+  let config = {
+    method: "post",
+    url: "https://api.openai.com/v1/audio/transcriptions",
+    data: data,
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+      "Content-Type": "multipart/form-data",
+    },
+  };
+
+  try {
+    const response = await axios.request(config);
+    if (response.status >= 200 && response.status < 300) {
+      return NextResponse.json(response.data);
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "An error occurred" },
+      {
+        status: error.response?.status || 500,
+        statusText: error.response?.statusText || "Internal Server Error",
+      }
+    );
+  }
+}
